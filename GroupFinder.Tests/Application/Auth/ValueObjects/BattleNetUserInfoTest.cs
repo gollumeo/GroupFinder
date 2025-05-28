a64d5da29d60d2846a4e3cc1dbe420a32daae091
Golluméo <p.mauriello54@gmail.com>
2025-05-29 01:57:17 +0200

    test(auth): add unit test for BattleNetUserInfo creation
    Added a test to verify the successful creation of BattleNetUserInfo with valid inputs. This ensures the correctness of object creation logic.

---
+++ GroupFinder.Tests/Application/Auth/ValueObjects/BattleNetUserInfoTest.cs
@@ -0,0 +1,23 @@
+﻿using GroupFinder.Application.Auth.ValueObjects;
+
+namespace GroupFinder.Tests.Application.Auth.ValueObjects;
+
+public class BattleNetUserInfoTest
+{
+    // TODO: add FluentAssertionsPackage
+    
+    [Fact]
+    public void CreatesSucceedsWithValidInputs()
+    {
+        const string id = "bnet-123456";
+        const string tag = "Thrall#1234";
+        const string region = "eu";
+        
+        var result = BattleNetUserInfo.Create(id, tag, region);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Id.Should().Be(id);
+        result.Value.BattleTag.Should().Be(tag);
+        result.Value.Region.Should().Be(tag);
+    }
+}
