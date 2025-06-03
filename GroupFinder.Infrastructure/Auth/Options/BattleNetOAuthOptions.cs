6731a56bc87017a356af09bf42063c3bbe15d988
Golluméo <p.mauriello54@gmail.com>
2025-06-03 15:55:09 +0200

    feat(auth): add properties to BattleNetOAuthOptions
    Introduce essential properties to `BattleNetOAuthOptions` for configuring Battle.net OAuth, including `ClientId`, `ClientSecret`, `AuthorizationUrl`, `RedirectUri`, `Scope`, and `ResponseType`. This prepares the options class to fully support OAuth configuration needs.

--- GroupFinder.Infrastructure/Auth/Options/BattleNetOAuthOptions.cs
+++ GroupFinder.Infrastructure/Auth/Options/BattleNetOAuthOptions.cs
@@ -2,5 +2,10 @@
 
 public class BattleNetOAuthOptions
 {
-    
+    public string ClientId { get; init; } = null!;
+    public string ClientSecret { get; init; } = null!;
+    public string AuthorizationUrl { get; init; } = null!;
+    public string RedirectUri { get; init; } = null!;
+    public string Scope { get; init; } = null!;
+    public string ResponseType { get; init; } = null!;
 }
