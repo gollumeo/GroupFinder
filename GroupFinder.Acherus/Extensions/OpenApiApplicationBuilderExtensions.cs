d987c93128a786837b9fb987e20e71900995fcef
Golluméo <p.mauriello54@gmail.com>
2025-06-06 01:36:29 +0200

    feat(GroupFinder): add OpenAPI extensions for Swagger configuration
    Introduce `OpenApiApplicationBuilderExtensions` class with a `MapOpenApi` method to configure Swagger middleware. This enables API documentation and provides a user-friendly interface for exploring API endpoints.

---
+++ GroupFinder.Acherus/Extensions/OpenApiApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace GroupFinder.Acherus.Extensions;
+
+public static class OpenApiApplicationBuilderExtensions
+{
+    public static void MapOpenApi(this WebApplication app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Group Finder API v1");
+            options.RoutePrefix = "swagger";
+        });
+    }
+}
