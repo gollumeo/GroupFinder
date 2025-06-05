837ec3994af170e20cf90ab44a3a56655fc99bdd
Golluméo <p.mauriello54@gmail.com>
2025-06-05 16:52:13 +0200

    feat(auth): add BattleNetTokenResponse model
    Introduce `BattleNetTokenResponse` sealed class representing the structure of the token response from Battle.net OAuth. This model includes properties such as `AccessToken`, `TokenType`, `ExpiresIn`, and `Scope`, essential for handling authentication responses.

---
+++ GroupFinder.Infrastructure/Auth/Models/BattleNetTokenResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace GroupFinder.Infrastructure.Auth.Models;
+
+public sealed class BattleNetTokenResponse
+{
+    public string AccessToken { get; set; } = null!;
+    public string TokenType { get; set; } = null!;
+    public int ExpiresIn { get; set; }
+    public string Scope { get; set; } = null!;
+}
