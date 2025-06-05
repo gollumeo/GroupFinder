8765a6fd1287ac14c2a817f6fed29e0f1d3bd151
Golluméo <p.mauriello54@gmail.com>
2025-06-06 01:37:42 +0200

    feat(GroupFinder): add Runeforge static class for modular app startup
    Introduce the `Runeforge` class with the `Inscribe` method to streamline and modularize the application startup process. This utilizes predefined `IRune` implementations for service configuration and app pipeline setup, improving scalability and maintainability.

---
+++ GroupFinder.Acherus/Runeforge.cs
@@ -0,0 +1,30 @@
+﻿using GroupFinder.Acherus.Contracts;
+using GroupFinder.Acherus.Modules;
+using Microsoft.AspNetCore.Builder;
+
+namespace GroupFinder.Acherus;
+
+public static class Runeforge
+{
+    public static async Task Inscribe(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        
+        foreach (var rune in Frostmourne())
+            rune.Extract(builder.Services, builder.Configuration);
+        
+        var app = builder.Build();
+
+        foreach (var rune in Frostmourne())
+            rune.CarveWith(app);
+
+        await app.RunAsync();
+    }
+
+    private static IEnumerable<IRune> Frostmourne()
+    {
+        yield return new HttpRune();
+        yield return new OpenApiRune();
+        yield return new AuthRune();
+    }
+}
