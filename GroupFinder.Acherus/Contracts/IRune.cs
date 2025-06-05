0e8bdb47d1c183c00a7480881b0002bc08af5bb3
Golluméo <p.mauriello54@gmail.com>
2025-06-06 01:34:31 +0200

    feat(GroupFinder): add IRune interface to Acherus contracts
    Introduce the `IRune` interface in the `GroupFinder.Acherus.Contracts` namespace. The interface defines methods for service extraction and application configuration, supporting dependency injection and initialization workflows.

---
+++ GroupFinder.Acherus/Contracts/IRune.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GroupFinder.Acherus.Contracts;
+
+public interface IRune
+{
+    void Extract(IServiceCollection services, IConfiguration config);
+
+    void CarveWith(WebApplication app);
+}
