967ba6c53ae9deb25dfb970306b17844d1302960
Golluméo <p.mauriello54@gmail.com>
2025-06-05 17:07:26 +0200

    feat(auth): add BattleNetUserInfoDto model
    Introduce `BattleNetUserInfoDto` class with properties `Id`, `BattleTag`, and `Region`. This sealed class provides a DTO representation for Battle.net user information to be used in the authentication flow.

---
+++ GroupFinder.Infrastructure/Auth/Models/BattleNetUserInfoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace GroupFinder.Infrastructure.Auth.Models;
+
+public sealed class BattleNetUserInfoDto
+{
+    public string Id { get; init; } = null!;
+    public string BattleTag { get; init; } = null!;
+    public string Region { get; init; } = null!;
+}
