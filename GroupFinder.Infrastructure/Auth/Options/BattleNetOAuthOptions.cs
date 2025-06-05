57a9764f1b3fe60fb97f87b97aafb05bc0cebdf7
Golluméo <p.mauriello54@gmail.com>
2025-06-05 16:52:39 +0200

    feat(auth): add additional URLs to BattleNetOAuthOptions
    Introduce `AuthorizationUrl`, `TokenUrl`, and `UserInfoUrl` properties to `BattleNetOAuthOptions`. These additions enable full configuration for OAuth endpoints necessary for the authentication flow with Battle.net.

--- GroupFinder.Infrastructure/Auth/Options/BattleNetOAuthOptions.cs
+++ GroupFinder.Infrastructure/Auth/Options/BattleNetOAuthOptions.cs
@@ -4,7 +4,11 @@ public class BattleNetOAuthOptions
 {
     public string ClientId { get; init; } = null!;
     public string ClientSecret { get; init; } = null!;
+    
     public string AuthorizationUrl { get; init; } = null!;
+    public string TokenUrl { get; init; } = null!;
+    public string UserInfoUrl { get; init; } = null!;
+    
     public string RedirectUri { get; init; } = null!;
     public string Scope { get; init; } = null!;
     public string ResponseType { get; init; } = null!;
