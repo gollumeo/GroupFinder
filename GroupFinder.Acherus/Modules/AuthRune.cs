7cbafde24bb2b146bad06fdb2f14b55a0c84b99c
Golluméo <p.mauriello54@gmail.com>
2025-06-06 01:36:12 +0200

    feat(auth): add AuthRune module to Acherus
    Introduce `AuthRune`, an implementation of the `IRune` interface for configuring authentication services. This includes registering `BattleNetAuthProxy` and related options for external authentication via Battle.net. Provides foundational setup for integrating Battle.net OAuth.

---
+++ GroupFinder.Acherus/Modules/AuthRune.cs
@@ -0,0 +1,27 @@
+﻿using GroupFinder.Acherus.Contracts;
+using GroupFinder.Application.Auth.Contracts;
+using GroupFinder.Infrastructure.Auth;
+using GroupFinder.Infrastructure.Auth.Options;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GroupFinder.Acherus.Modules;
+
+public class AuthRune : IRune
+{
+    public void Extract(IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<BattleNetOAuthOptions>(
+            config.GetSection("BattleNetOAuth")
+        );
+
+        services.AddHttpClient<BattleNetAuthProxy>();
+        services.AddScoped<IExternalAuthentication, BattleNetAuthProxy>();
+    }
+
+    public void CarveWith(WebApplication app)
+    {
+        // No middleware to boot here, yet
+    }
+}
