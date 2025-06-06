3dbee18d1972cf8a0631772fd96880faf13b3baf
Golluméo <p.mauriello54@gmail.com>
2025-06-06 13:41:01 +0200

    feat(auth): add IBattleNetApi contract
    Introduce `IBattleNetApi` interface in the `Auth.Contracts` namespace. Defines methods for exchanging authorization codes for tokens and fetching Battle.Net user information, improving modularity and integration with external authentication services.

---
+++ GroupFinder.Infrastructure/Auth/Contracts/IBattleNetApi.cs
@@ -0,0 +1,9 @@
+﻿using GroupFinder.Infrastructure.Auth.Models;
+
+namespace GroupFinder.Infrastructure.Auth.Contracts;
+
+public interface IBattleNetApi
+{
+    Task<BattleNetTokenResponse> ExchangeCodeForToken(string code, string redirectUri);
+    Task<BattleNetUserInfoDto> FetchUserInfo(string accessToken);
+}
