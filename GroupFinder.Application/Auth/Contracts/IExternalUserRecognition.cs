7f784c53c03e7c0d21da0e8ce71a1a4741661b24
Golluméo <p.mauriello54@gmail.com>
2025-06-06 13:18:02 +0200

    feat(auth): add IExternalUserRecognition contract
    Introduce `IExternalUserRecognition` interface in the `Auth.Contracts` namespace. Defines a contract for recognizing external users based on provided information, returning a result encapsulating recognized user details. Supports extensibility and separation of concerns in the authentication module.

---
+++ GroupFinder.Application/Auth/Contracts/IExternalUserRecognition.cs
@@ -0,0 +1,10 @@
+﻿using GroupFinder.Application.Auth.DTO;
+using GroupFinder.Application.Auth.ValueObjects;
+using GroupFinder.Application.Common;
+
+namespace GroupFinder.Application.Auth.Contracts;
+
+public interface IExternalUserRecognition
+{
+    Task<Result<RecognizedUser>> From(ExternalUserInfo info);
+}
