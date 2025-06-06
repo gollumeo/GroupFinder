e09215e85cfcf50895d658fcae448f97a5957c5e
Golluméo <p.mauriello54@gmail.com>
2025-06-06 13:17:35 +0200

    test(auth): add unit tests for RecognizeExternalUser use case
    Implement unit tests for the `RecognizeExternalUser` use case, covering success and failure scenarios. This includes tests for valid external user input and early exits when the input is invalid, ensuring robust behavior and error handling.

--- GroupFinder.Tests/Application/Auth/RecognizeExternalUserTest.cs
+++ GroupFinder.Tests/Application/Auth/RecognizeExternalUserTest.cs
@@ -1,11 +1,31 @@
-﻿namespace GroupFinder.Tests.Application.Auth;
+﻿using FluentAssertions;
+using GroupFinder.Application.Auth;
+using GroupFinder.Application.Auth.Contracts;
+using GroupFinder.Application.Auth.DTO;
+using GroupFinder.Application.Auth.ValueObjects;
+using GroupFinder.Application.Common;
+using Moq;
+
+namespace GroupFinder.Tests.Application.Auth;
 
 public class RecognizeExternalUserTest
 {
     [Fact]
     public async Task ReturnsRecognizedExternalUser()
     {
-        await Task.CompletedTask;
+        var externalInfo = new ExternalUserInfo("ext-123", "DarkDk#0666", "eu");
+        
+        var fakeUserId = Guid.NewGuid();
+
+        var gateway = new Mock<IExternalUserRecognition>();
+        gateway.Setup(x => x.From(externalInfo))
+            .ReturnsAsync(Result<RecognizedUser>.Success(new RecognizedUser(fakeUserId)));
+
+        var useCase = new RecognizeExternalUser(gateway.Object);
+        var result = await useCase.Execute(externalInfo);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().Be(fakeUserId);
     }
     
     [Fact]
@@ -14,9 +34,25 @@ public class RecognizeExternalUserTest
         await Task.CompletedTask;
     }
 
-    [Fact]
-    public async Task ReturnsEarlyWithInvalidInput()
+    [Theory]
+    [InlineData("id-123", "Waza#1234", "na")]
+    [InlineData("", "Waza#1234", "eu")]
+    [InlineData("", "", "eu")]
+    [InlineData("", "", "")]
+    public async Task ReturnsEarlyWithInvalidInput(string id, string battleTag, string region)
     {
-        await Task.CompletedTask;
+        var externalInfo = new ExternalUserInfo(id, battleTag, region);
+        
+        var recognition = new Mock<IExternalUserRecognition>();
+        recognition.Setup(x => x.From(It.IsAny<ExternalUserInfo>()))
+            .ReturnsAsync(Result<RecognizedUser>.Failure("Invalid external user info."));
+        
+        var useCase = new RecognizeExternalUser(recognition.Object);
+        var result = await useCase.Execute(externalInfo);
+        
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Contain("Invalid external user info.");
+        
+        recognition.Verify(x => x.From(It.IsAny<ExternalUserInfo>()), Times.Never);
     }
 }
