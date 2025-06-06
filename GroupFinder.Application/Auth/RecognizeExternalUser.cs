bcff8647f0a4efec31e4980fdf094ffc75a3308d
Golluméo <p.mauriello54@gmail.com>
2025-06-06 22:17:45 +0200

    refactor(auth): remove redundant input validation in RecognizeExternalUser
    Eliminate duplicate input validation logic in `RecognizeExternalUser`. Streamlines the use case to improve code clarity and focus on essential responsibility, as input validation is already handled externally.

--- GroupFinder.Application/Auth/RecognizeExternalUser.cs
+++ GroupFinder.Application/Auth/RecognizeExternalUser.cs
@@ -8,13 +8,8 @@ public class RecognizeExternalUser(IExternalUserRecognition recognition)
 {
     public async Task<Result<Guid>> Execute(ExternalUserInfo info)
     {
-        if (string.IsNullOrWhiteSpace(info.Id) || string.IsNullOrWhiteSpace(info.BattleTag) ||
-            string.IsNullOrWhiteSpace(info.Region))
-            return Result<Guid>.Failure("Invalid external user info.");
-
         var result = await recognition.From(info);
 
-        // ReSharper disable once ConvertIfStatementToReturnStatement
         if (result.IsFailure)
             return Result<Guid>.Failure(result.Error);
 
