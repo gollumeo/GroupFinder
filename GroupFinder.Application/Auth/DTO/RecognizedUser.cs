f547c94079e61b79784cf5123df436117fe41be2
Golluméo <p.mauriello54@gmail.com>
2025-06-07 03:01:18 +0200

    feat(auth): extend RecognizedUser with BattleTag and Region
    Add `BattleTag` and `Region` properties to the `RecognizedUser` DTO to support additional user information. This change lays the groundwork for enhanced functionality and compatibility with external systems requiring these details.

--- GroupFinder.Application/Auth/DTO/RecognizedUser.cs
+++ GroupFinder.Application/Auth/DTO/RecognizedUser.cs
@@ -1,6 +1,8 @@
 ﻿namespace GroupFinder.Application.Auth.DTO;
 
-public sealed class RecognizedUser(Guid id)
+public sealed class RecognizedUser(Guid id, string battleTag, string region)
 {
     public Guid Id { get; } = id;
+    public string BattleTag { get; } = battleTag;
+    public string Region { get; } = region;
 }
