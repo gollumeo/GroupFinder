77c6a9c0becbf2d2f145e95bf8fdbb11f9860560
Golluméo <p.mauriello54@gmail.com>
2025-06-07 03:01:32 +0200

    test(auth): add test for AuthenticateUserViaSso use case
    Introduce `AuthenticateUserViaSsoTest` to verify user creation on first login via SSO. Includes validation for external ID, BattleTag, and region mapping. This improves test coverage for the `AuthenticateUserViaSso` use case.

---
+++ GroupFinder.Tests/Application/Auth/AuthenticateUserViaSsoTest.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using GroupFinder.Application;
+using GroupFinder.Application.Auth.ValueObjects;
+using GroupFinder.Persistence.Repositories;
+
+namespace GroupFinder.Tests.Application.Auth;
+
+public class AuthenticateUserViaSsoTest
+{
+    [Fact]
+    public async Task CreatesANewUserOnFirstLogin()
+    {
+        // Given
+        var users = new InMemoryUsers();
+        var useCase = new AuthenticateUserViaSso(users);
+
+        var info = new ExternalUserInfo(
+            "bnet-123456",
+            "Thrall#1234",
+            "eu"
+        );
+
+        // When
+        var result = await useCase.Execute(info);
+
+        // Then
+        result.Should().NotBeNull();
+        result.Value.Id.Should().NotBeEmpty();
+
+        // TODO: handle dictionary populating strategy
+        var user = await users.FindByExternalId("bnet-123456");
+
+        user.Should().NotBeNull();
+        user.Value.BattleTag.Should().Be("Thrall#1234");
+        user.Value.Region.Should().Be("eu");
+    }
+}
